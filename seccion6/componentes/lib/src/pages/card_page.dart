af8c5e7272924c05245c1ccb685a4df31d0d02cd
David Robinson <david.robinson@logicstudio.net>
2020-09-27 13:24:54 -0500

    feat(seccion 6): card sencilla

---
+++ seccion6/componentes/lib/src/pages/card_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: [_cardTipo1()],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(
+              Icons.photo_album,
+              color: Colors.blue,
+            ),
+            title: Text('Soy el titulo'),
+            subtitle: Text('ssssssssssssssssssssssssssssssssssssssssssssssss'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {},
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}

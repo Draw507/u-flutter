cc95fbdc94bde4586c901c0ba615a4c8c03d05d5
David Robinson <david.robinson@logicstudio.net>
2020-09-27 19:50:35 -0500

    feat(peliculas): Horizontal PageView

--- seccion7/peliculas/lib/src/pages/home_page.dart
+++ seccion7/peliculas/lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/providers/peliculas_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 class HomePage extends StatelessWidget {
   final peliculasProvider = PeliculasProvider();
@@ -15,7 +16,8 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
         child: Column(
-          children: [_swiperTarjetas(), _footer()],
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [_swiperTarjetas(), _footer(context)],
         ),
       ),
     );
@@ -26,7 +28,6 @@ class HomePage extends StatelessWidget {
       future: peliculasProvider.getEnCines(),
       builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
         if (snapshot.hasData) {
-          print(snapshot.hasData);
           return CardSwiper(
             peliculas: snapshot.data,
           );
@@ -38,10 +39,36 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _footer() {
+  Widget _footer(BuildContext context) {
     return Container(
       width: double.infinity,
-      child: Column(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(left: 20.0),
+            child: Text(
+              'Populares',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          FutureBuilder(
+            future: peliculasProvider.getPopulares(),
+            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+              if (snapshot.hasData) {
+                return MovieHorizontal(
+                  peliculas: snapshot.data,
+                );
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }

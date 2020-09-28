800effb15beee1085a19d314ed7de94f4ddc094e
David Robinson <david.robinson@logicstudio.net>
2020-09-28 16:08:31 -0500

    feat(login): Mantener data de los Streams

--- seccion11/formvalidation/lib/src/blocs/provider.dart
+++ seccion11/formvalidation/lib/src/blocs/provider.dart
@@ -3,9 +3,18 @@ import 'package:formvalidation/src/blocs/login_bloc.dart';
 export 'package:formvalidation/src/blocs/login_bloc.dart';
 
 class Provider extends InheritedWidget {
-  final loginBloc = LoginBloc();
+  static Provider _instancia;
+  factory Provider({Key key, Widget child}) {
+    if (_instancia == null) {
+      _instancia = new Provider._internal(key: key, child: child);
+    }
 
-  Provider({Key key, Widget child}) : super(key: key, child: child);
+    return _instancia;
+  }
+
+  Provider._internal({Key key, Widget child}) : super(key: key, child: child);
+
+  final loginBloc = LoginBloc();
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;

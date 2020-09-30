298426e8f179bf93b5ba6613e34e55e7df2032ab
David Robinson <david.robinson@logicstudio.net>
2020-09-30 11:05:08 -0500

    feat(provider): configuracion del proyecto

---
+++ seccion17/provider/lib/src/pages/tabs_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class TabsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _Paginas(),
+      bottomNavigationBar: _Navegacion(),
+    );
+  }
+}
+
+class _Navegacion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 1,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          title: Text('Para ti'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.public),
+          title: Text('Encabezados'),
+        ),
+      ],
+    );
+  }
+}
+
+class _Paginas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      //physics: BouncingScrollPhysics(),
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        Container(),
+        Container(),
+      ],
+    );
+  }
+}
